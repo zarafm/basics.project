f50e201d2f5e040b985b44d53fa2c439840dfae4
Shahruq <Shahruq@users.noreply.github.com>
2019-01-09 23:45:45 +0000

    lm analysis for nested dataframes or stratified datasets

--- 1.R
+++ 1.R
@@ -1,3 +1,28 @@
-a=1
-a=2
-a=3
+##to avoid for loops we use nested dataframes. This is particularly useful for stratified datasets - with categorical variables 
+
+gapminder.nested <- gapminder %>% 
+  group_by(country, continent) %>%
+  nest()
+gapminder.nested ## this will create a nested dataframe
+
+#################################################
+
+country_model <- function(df){
+  lm(lifeExp ~ year, data = df)
+} ## this will create a function which carries out linear model (regression analysis) for all the countries
+
+###############################################
+
+gapminder.model <- gapminder.nested %>%
+  mutate(model = map(data, country_model),
+         coef = map(model, broom::tidy)) ## this will map the nested dataframe with the analysis and now we have analysis for all the countries.
+
+###############################################
+
+#test using
+
+gapminder.model$coef[[1]] ###this is a way to test the function
+
+##if you want to view data of a particular country then
+
+gapminder.nested[1,]$data
